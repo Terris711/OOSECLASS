13d6e363021d5984b33a178ea3bc749799e8a854
Thi Van Anh Duong <anht@cbh.com.au>
2024-03-13 12:57:26 +0800

    Activity 3

--- src/Week1/worksheet1_addressbook_java/src/main/java/edu/curtin/addressbook/Main.java
+++
@@ -1,7 +0,0 @@
-package Week1.worksheet1_addressbook_java.src.main.java.edu.curtin.addressbook;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
