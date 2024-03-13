d866c54444863ef800d87329425cc695b4411a19
Thi Van Anh Duong <anht@cbh.com.au>
2024-03-13 12:58:39 +0800

    Activity 3

---
+++ src/Week1/worksheet1_imageviewer_java/src/main/java/edu/curtin/imageviewer/Album.java
@@ -0,0 +1,10 @@
+package Week1.worksheet1_imageviewer_java.src.main.java.edu.curtin.imageviewer;
+import java.util.*;
+
+/**
+ * Represents an album of images.
+ */
+public class Album 
+{
+    // FIXME: Insert your fields, constructors and methods here.
+}
---
+++ src/Week1/worksheet1_imageviewer_java/src/main/java/edu/curtin/imageviewer/ImageRecord.java
@@ -0,0 +1,27 @@
+package Week1.worksheet1_imageviewer_java.src.main.java.edu.curtin.imageviewer;
+
+/**
+ * Represents an image in an album.
+ */
+public class ImageRecord
+{
+    private String filename;
+    private String caption;
+    
+    public ImageRecord(String newFilename, String newCaption)
+    {
+        filename = newFilename;
+        caption = newCaption;
+    }
+    
+    public String getFilename()
+    {
+        return filename;
+    }
+    
+    public String getCaption()
+    {
+        return caption;
+    }
+}
+
